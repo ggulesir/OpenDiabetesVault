3d2b67292c88a3fb741ed80dc24aeec34b61aa32
Gizem <gizemgulesir@gmail.com>
2017-09-01 10:40:35 +0200

    forgot to stage while previous commit

--- engine/src/de/jhit/opendiabetes/vault/processing/StaticInsulinSensivityCalculator.java
+++ engine/src/de/jhit/opendiabetes/vault/processing/StaticInsulinSensivityCalculator.java
@@ -22,15 +22,12 @@ import static de.jhit.opendiabetes.vault.container.VaultEntryType.BOLUS_NORMAL;
 import static de.jhit.opendiabetes.vault.container.VaultEntryType.EXERCISE_MANUAL;
 import static de.jhit.opendiabetes.vault.container.VaultEntryType.GLUCOSE_CGM;
 import static de.jhit.opendiabetes.vault.container.VaultEntryType.MEAL_MANUAL;
-import static de.jhit.opendiabetes.vault.importer.validator.MedtronicCsvValidator.TYPE.BOLUS_SQUARE;
-import de.jhit.opendiabetes.vault.processing.filter.TimePointFilter;
+import static de.jhit.opendiabetes.vault.container.VaultEntryType.BOLUS_SQUARE;
+import de.jhit.opendiabetes.vault.processing.filter.DateTimePointFilter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javafx.util.Pair;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
 import java.util.Iterator;
 import static java.util.concurrent.TimeUnit.*;
 
@@ -45,7 +42,7 @@ public class StaticInsulinSensivityCalculator {
     public StaticInsulinSensivityCalculator(StaticInsulinSensivityCalculatorOptions options) {
         this.options = options;
     }
-
+    
     /**
      * Calculate insulin sensitivity from given data
      *
@@ -55,79 +52,79 @@ public class StaticInsulinSensivityCalculator {
      */
     public List<Pair<Date, Double>> calculateFromData(List<VaultEntry> data) {
         List<Pair<Date, Double>> retVal = new ArrayList<>();
-        List<List<VaultEntry>> cutTimeSeries = new ArrayList<>();
+        List<Pair<Date, List<VaultEntry>>> cutTimeSeries = new ArrayList<>();
         int bolusCount;
-        boolean bolusFound, cgmBeginFound;
+        boolean bolusFound, cgmBeginFound, otherFound = false;
         Date bolusDate;
         double bolusVal, cgmBegin, cgmEnd;
-        
+        double bolusAdd = 0.0;
         FilterEvent eventFilter = new FilterEvent();
         VaultEntryType type = BOLUS_NORMAL;
         // Save timestamp of each bolus event
         List<Date> bolusEvents = eventFilter.filter(data, type, options.range);
-        
+
         // Cut time series including bolus event in middle
         for (Date date : bolusEvents) {
-            LocalTime localTime = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()).toLocalTime();
-            TimePointFilter filter = new TimePointFilter(localTime, (int) options.range);
-            cutTimeSeries.add((filter.filter(data)).filteredData);
-        }
-
-       double bolusAdd = 0.0;
-        // Merging bolus events within max 30 min distance
-        long span = MILLISECONDS.convert(options.bolusSpan, MINUTES);
-        for (List<VaultEntry> timeSerie : cutTimeSeries) {
-            for (Date blsDate : bolusEvents) {
-                for (Iterator<VaultEntry> iterator = timeSerie.iterator(); iterator.hasNext();) {
-                    VaultEntry entry = iterator.next();
-                    if (entry.getType().equals(BOLUS_NORMAL)) {
-                        long diffInMillies = entry.getTimestamp().getTime() - blsDate.getTime();
-                        // Next bolus events within the bolusSpan
-                        if (diffInMillies < span && diffInMillies > 0) {
-                            // Retrive bolus value to be added to previous bolus event
-                            double current = bolusAdd;
-                            bolusAdd = entry.getValue() + current;
-                            // Remove this bolus event inside bolusSpan from the cutTimeSeries
-                            iterator.remove();
-                            // OR dont remove, later ignore bolus events within bolusSpan
-                        }
-                    }
-                }
-                // Merge bolus value here and initialize it for next cut time serie (next bolus cluster)
-                for (Iterator<VaultEntry> it = timeSerie.iterator(); it.hasNext();) {
-                    VaultEntry ent = it.next();
-                    if (ent.getType().equals(BOLUS_NORMAL) && ent.getTimestamp().equals(blsDate)) {
-                       double current = ent.getValue();
-                       ent.setValue(current + bolusAdd);
-                       bolusAdd = 0.0; 
-                    }
-                }
-                
-            }
+            DateTimePointFilter filter = new DateTimePointFilter(date, (int) options.range);
+            cutTimeSeries.add(new Pair(date, (filter.filter(data)).filteredData));
         }
         
+        
+        // Merging bolus events within max bolusSpan mins distance
+        long span = MILLISECONDS.convert(options.bolusSpan, MINUTES);
+        for (Pair<Date, List<VaultEntry>> pair : cutTimeSeries) {
+            Date bolus = pair.getKey();
+            for (Iterator <VaultEntry> iterator = pair.getValue().iterator(); iterator.hasNext();) {
+                VaultEntry entry = iterator.next();
+                if (entry.getType().equals(BOLUS_NORMAL)) {
+                    long diffInMillies = entry.getTimestamp().getTime() - bolus.getTime();
+                    // Next bolus events within the bolusSpan
+                    if (diffInMillies < span && diffInMillies > 0) {
+                        // Retrive bolus value to be added to previous bolus event
+                        double current = bolusAdd;
+                        bolusAdd = entry.getValue() + current;
+                        // Remove this bolus event inside bolusSpan from the cutTimeSeries
+                        iterator.remove();
+                        // OR dont remove, later ignore bolus events within bolusSpan
+                    }
+                }
+            }
+            // Merge bolus value here and initialize it for next cut time serie (next bolus cluster)
+            for (Iterator <VaultEntry> it = pair.getValue().iterator(); it.hasNext();) {
+                VaultEntry entry = it.next();
+                if (entry.getType().equals(BOLUS_NORMAL) && entry.getTimestamp().equals(bolus)) {
+                    double current = entry.getValue();
+                    entry.setValue(current + bolusAdd);
+                    bolusAdd = 0.0;
+                }
+            }
+        }
+ 
         //Check each sub timeseries whether they have another bolus or any meal event within the range.
-        for(Iterator<List<VaultEntry>> iterator = cutTimeSeries.iterator(); iterator.hasNext();){
+        for (Iterator <Pair<Date, List<VaultEntry>>> iterator = cutTimeSeries.iterator(); iterator.hasNext();) {
+            Pair <Date, List<VaultEntry>> pair = iterator.next();
+            Date bolus = pair.getKey();
             bolusCount = 0;
-            List<VaultEntry> listEntry = iterator.next();
-            for (VaultEntry entry : listEntry) {
-              if (entry.getType().equals(BOLUS_NORMAL)) {
+            otherFound = false;
+            for (VaultEntry entry : pair.getValue()) {
+                if (entry.getType().equals(BOLUS_NORMAL)) {
                     bolusCount++;
-              }
-              if (entry.getType().equals(MEAL_MANUAL) || entry.getType().equals(BOLUS_SQUARE)
+                }
+                if (entry.getType().equals(MEAL_MANUAL) || entry.getType().equals(BOLUS_SQUARE)
                       || entry.getType().equals(EXERCISE_MANUAL)) {
-                iterator.remove();
-              }
+                    otherFound = true;
+                }
             }
-            if (bolusCount > 1) {
+            if (bolusCount > 1 || otherFound) {
                 iterator.remove();
             }
-            
         }
         
         //Remaining sets, elect where cgm is decreasing after bolus event
-        //Looking for delta, (cgmT+delayedStart - cgmT+2h) < 0 wıth delayed start in mins
-        for (Iterator<List<VaultEntry>> iterator = cutTimeSeries.iterator(); iterator.hasNext();) {
+        //Looking for delta, (cgmT+delayedStart - cgmT+range) < 0 wıth delayed start in mins
+        for (Iterator <Pair<Date, List<VaultEntry>>> iterator = cutTimeSeries.iterator(); iterator.hasNext();) {
+            Pair <Date, List<VaultEntry>> pair = iterator.next();
+            Date bolus = pair.getKey();
             // Initialize variables for next time series
             bolusFound = false;
             cgmBeginFound = false;
@@ -136,10 +133,9 @@ public class StaticInsulinSensivityCalculator {
             cgmBegin = 0.0;
             cgmEnd = 0.0;
             long delayedStart = MILLISECONDS.convert(options.cgmDelayedStart, MINUTES);
-            List<VaultEntry> listEntry = iterator.next();
-            for (VaultEntry entry : listEntry) {
+            for (VaultEntry entry : pair.getValue()) {
                 // Bolus event is found, save the bolus value for later calculation
-                if (entry.getType().equals(BOLUS_NORMAL) && !bolusFound) {
+                if (entry.getType().equals(BOLUS_NORMAL) && !bolusFound && entry.getTimestamp().equals(bolus)) {
                     bolusFound = true;
                     bolusDate = entry.getTimestamp();
                     bolusVal = entry.getValue();
@@ -163,8 +159,8 @@ public class StaticInsulinSensivityCalculator {
             }
             //calculate sensitivity value, only add into list if delta is < 0 (decreasing cgm)
             if (bolusFound && cgmBeginFound && (cgmEnd - cgmBegin < 0) && bolusVal != 0.0) {
-                Pair <Date, Double> pair = new Pair <>(bolusDate, (cgmEnd-cgmBegin)/bolusVal);
-                retVal.add(pair);
+                Pair <Date, Double> newPair = new Pair <>(bolusDate, (cgmEnd-cgmBegin)/bolusVal);
+                retVal.add(newPair);
             }
         }
         return retVal;
